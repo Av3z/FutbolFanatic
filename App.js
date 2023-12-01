45927d548972ce1b88c060448b8a4fb07b9bffd2
Av3z <willyanf17@hotmail.com>
2023-12-01 16:28:12 -0300

    feat: implemented routes in app.js

--- App.js
+++ App.js
@@ -1,33 +1,12 @@
-import React, {useState, useEffect} from 'react';
-import { FlatList} from 'react-native';
-
-import api from './src/services/api';
-import Rank from './src/rank';
-
-import { RankHeader } from './src/components/rankHeader';
-import { StyledView } from './src/components/StyledComponents';
+import { NavigationContainer } from "@react-navigation/native"
+import { Routes } from './src/Routes'
 
 export default function App(){
 
-
-  const [rank, setRank] = useState([]);
-
-  useEffect(() => {
-    api.get('campeonatos/10/tabela').then(({data}) => {
-      setRank(data)
-    });
-  }, [])
-
   return(
-    <StyledView className='flex-1 mt-10 h-screen w-full bg-slate-500'>
-        <FlatList 
-          data={rank}
-          keyExtractor={item => item.posicao}
-          ListHeaderComponent={() => <RankHeader />}
-          renderItem={({item}) => <Rank data={item}/>}
-        />
-
-    </StyledView>
+  <NavigationContainer>
+    <Routes />
+  </NavigationContainer>
   )
 }
 
