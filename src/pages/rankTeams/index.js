56de68c0453dce768cd622b647368463648dbb51
Av3z <willyanf17@hotmail.com>
2023-12-02 02:06:41 -0300

    refactor: implemented array columns for make a RankHeader more dynamic

--- src/pages/rankTeams/index.js
+++ src/pages/rankTeams/index.js
@@ -2,10 +2,10 @@ import React, {useState, useEffect} from 'react';
 import { FlatList} from 'react-native';
 
 import api from '../../services/api';
-import Rank from '../../rank';
 
 import { RankHeader } from '../../components/rankHeader';
 import { StyledView } from '../../components/StyledComponents';
+import RankTeamsTable from '../../components/rankTeamsTable';
 
 export function RankTeams(){
 
@@ -17,13 +17,24 @@ export function RankTeams(){
     });
   }, [])
 
+  const columns = [
+    { flex: 1, title: 'POS' },
+    { flex: 5, title: 'TIME' },
+    { flex: 1, title: 'PTS' },
+    { flex: 1, title: 'J' },
+    { flex: 1, title: 'V' },
+    { flex: 1, title: 'E' },
+    { flex: 1, title: 'D' },
+    { flex: 1, title: 'SG' },
+  ];
+
   return(
-    <StyledView className='flex-1 mt-10 h-screen w-full bg-slate-500'>
+    <StyledView className='flex mt-10'>
         <FlatList 
           data={rank}
           keyExtractor={item => item.posicao}
-          ListHeaderComponent={() => <RankHeader />}
-          renderItem={({item}) => <Rank data={item}/>}
+          ListHeaderComponent={() => <RankHeader columns={columns} />}
+          renderItem={({item}) => <RankTeamsTable data={item}/>}
         />
     </StyledView>
   )
