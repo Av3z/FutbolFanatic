de319a4ae6051904106bb3182d62389b5bc440c1
Av3z <willyanf17@hotmail.com>
2023-12-02 02:05:13 -0300

    feat: implemented all code for show rank players

--- src/pages/rankPlayers/index.js
+++ src/pages/rankPlayers/index.js
@@ -1,9 +1,37 @@
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import api from '../../services/api'
+import { StyledView } from "../../components/StyledComponents";
+import { FlatList } from "react-native";
+import { RankPlayersTable } from "../../components/RankPlayersTable";
+import { RankHeader } from '../../components/rankHeader';
+
 
 export function RankPlayers(){
+    
+    const [rankPlayers, setRankPlayers] = useState([]);
+
+    useEffect(() =>{
+        api.get('campeonatos/10/artilharia').then(({ data }) =>{
+            setRankPlayers(data);
+        });
+    },[])
+
+    const columns = [
+        { flex: 2, title: 'TIME' },
+        { flex: 2, title: 'JOGADOR' },
+        { flex: 1, title: 'GOLS' },
+    ]
+
+    console.log(rankPlayers)
+
     return(
-        <View>
-            <Text> Artilharia </Text>
-        </View>
+        <StyledView className="mt-10">
+            <FlatList 
+                data={rankPlayers}
+                keyExtractor={item => item.atleta.atleta_id}
+                ListHeaderComponent={() => <RankHeader  columns={columns} />}
+                renderItem={({item}) => <RankPlayersTable data={item}/>}
+            />
+        </StyledView>
     )
 }
