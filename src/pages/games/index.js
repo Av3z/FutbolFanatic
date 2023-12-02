870cbf8e7872136ae2001ffe0d56dd05869779b1
Av3z <willyanf17@hotmail.com>
2023-12-02 02:03:27 -0300

    feat: implemented all code

--- src/pages/games/index.js
+++ src/pages/games/index.js
@@ -1,9 +1,27 @@
-import { View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { View, FlatList } from "react-native";
+import api from "../../services/api";
+import { Matches } from "../../components/Matches";
 
 export function Games(){
+
+    const [games, setGames] = useState([]);
+
+    useEffect(() =>{
+        api.get('ao-vivo').then(({ data }) =>{
+            setGames(data)
+        })
+    },[])
+
+    console.log(games)
+
     return(
         <View>
-            <Text> Tabela de jogos </Text>
+            <FlatList
+                data={games}
+                keyExtractor={item => item.partida_id}
+                renderItem={({ item }) => <Matches data={item}/> }
+            />
         </View>
     )
 }
